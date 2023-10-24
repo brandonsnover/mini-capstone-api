30c5b82ef196745e4b17330893b0cb49e90935cd
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-24 11:59:48 -0500

    routes of individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,16 @@
 class ProductsController < ApplicationController
+  def all_products
+    @products = Product.all
+    render :index
+  end
+
+  def one_product
+    @product = Product.first
+    render :show
+  end
+
+  def hammer
+    @product = Product.find_by(name: "Hammer")
+    render :show
+  end
 end
