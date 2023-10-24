385a475a22e91df668e274f89f95e66395cfd171
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-24 14:11:48 -0500

    added id param

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
-  get "/one_product", controller: "products", action: "one_product"
+  get "/one_product/:id", controller: "products", action: "one_product"
   get "/hammer", controller: "products", action: "hammer"
 end
