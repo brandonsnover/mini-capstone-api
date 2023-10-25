9167a15f28c80fc0fc1059832cea8adc3b432a48
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-25 13:00:49 -0500

    added create action and route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,10 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @product = Product.new(name: "Turtle", price: 11, image_url: "turtle.png", description: "its slow...")
+    @product.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/products.json", controller: "products", action: "index"
   get "/products/:id.json", controller: "products", action: "show"
+  post "/products.json", controller: "products", action: "create"
 end
