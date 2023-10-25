234b56d02c108e1051c6ff2e1cdd1bb09a19302a
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-25 12:20:08 -0500

    added test and edit jbuilder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,11 @@
 class ProductsController < ApplicationController
-  def all_products
+  def index
     @products = Product.all
     render :index
   end
 
-  def one_product
+  def show
     @product = Product.find_by(id: params["id"])
-    render :show
-  end
-
-  def hammer
-    @product = Product.find_by(name: "Hammer")
     render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "/all_products", controller: "products", action: "all_products"
-  get "/one_product/:id", controller: "products", action: "one_product"
-  get "/hammer", controller: "products", action: "hammer"
+  get "/products.json", controller: "products", action: "index"
+  get "/products/:id.json", controller: "products", action: "show"
 end
