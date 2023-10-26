aa9d45c5da5f81d347146df0a200fe71e36c354c
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-26 15:05:05 -0500

    added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,11 +9,11 @@ class Product < ApplicationRecord
 
   def tax
     tax = (price * 0.09)
-    tax
+    tax.round(2)
   end
 
   def total
-    total = price + (price * 0.09)
-    total
+    total = price * 1.09
+    total.round(2)
   end
 end
