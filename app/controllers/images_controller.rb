9701e1b080fc513843b0e374fe7f1cf96a9a2ea0
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-31 15:07:56 -0500

    added image index and show controller/route

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,14 @@
 class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+    render :index
+  end
+
+  def show
+    @image = Image.find_by(id: params["id"])
+    render :show
+  end
+
   def create
     @image = Image.new(url: params["url"], product_id: params["product_id"])
     @image.save
