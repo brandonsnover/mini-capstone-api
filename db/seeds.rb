691484c6008c084e41f9f14ac01abb477ba1a904
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-31 12:47:10 -0500

    added product.images method

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,14 @@
 # product = Product.new(name: "Paint", price: 8, image_url: "imageurl", description: "its red")
 # product.save
 
-Supplier.create(name: "Gomart", email: "gmart@email.com", phone_number: "555-1234")
+# Supplier.create(name: "Gomart", email: "gmart@email.com", phone_number: "555-1234")
 
-Supplier.create(name: "Smartmart", email: "smart@email.com", phone_number: "555-4321")
+# Supplier.create(name: "Smartmart", email: "smart@email.com", phone_number: "555-4321")
+
+Image.create(url: "example_url", product_id: 2)
+
+Image.create(url: "example_url4", product_id: 5)
+
+Image.create(url: "example_url5", product_id: 3)
+
+Image.create(url: "example_url2", product_id: 2)
