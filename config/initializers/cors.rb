1379a87961888ac194a4bca9086383fcd58a70de
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-12-08 13:02:25 -0600

    updates cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "https://mini-capstone-frontend-kkfd.onrender.com"
+    origins "localhost:5173", "https://mini-capstone-api-ud5u.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
